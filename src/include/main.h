169a33a3fcf979792824cd341bdca5d039b51ff6
06712L <bananah2odev@gmail.com>
2026-09-23 19:19:18 +0800

    feat(cli): add CONTINUE_RUN enum through main.h
    We need an enumeration value to determine whether main.c should continue running or return a function's return value

---
+++ src/include/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+enum
+{
+    //If this value is returned main.c will continue running instead of returning the function's return value
+    CONTINUE_RUN = 67,
+};
+
+#endif
